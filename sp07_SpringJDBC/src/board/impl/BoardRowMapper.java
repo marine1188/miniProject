1ef1dae48581cb9dce698c73a29cd93da3801ab3
marine1188 <marine1188@yahoo.com>
2021-05-27 13:55:51 +0900

    스프링 JDBC 자바 코드 DAO 활용

---
+++ sp07_SpringJDBC/src/board/impl/BoardRowMapper.java
@@ -0,0 +1,21 @@
+package board.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import board.BoardVO;
+
+public class BoardRowMapper implements RowMapper<BoardVO> {
+	public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		BoardVO board = new BoardVO();
+		board.setSeq(rs.getInt("SEQ"));
+		board.setTitle(rs.getString("TITLE"));
+		board.setWriter(rs.getString("WRITER"));
+		board.setContent(rs.getString("CONTENT"));
+		board.setRegDate(rs.getDate("REGDATE"));
+		board.setCnt(rs.getInt("CNT"));
+		return board;
+	}
+}
